58586cd39d72711c8d1677f7a0758f7d1d19d3c8
Alyxhelpme <74560600+Alyxhelpme@users.noreply.github.com>
2025-04-01 16:02:16 -0600

    All files in the modules are important so we'll be including all of them

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "License :: OSI Approved :: MIT License",
     ],
     package_data={
-        "": ["*.json","*.txt"],  # Include all JSON and txt files in all packages
+        "": ["*.*"],  # Include all JSON and txt files in all packages
     },
     include_package_data=True,
 )
